b95f83244e90ce0fa06f7c8a91effca89154a80f
Puld <mr.puld@gmail.com>
2021-02-10 16:07:17 +0300

    #6215

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,25 +3,6 @@ module.exports = {
 	ssr: false,
 	modules: [
 		'@nuxtjs/pwa',
-		['nuxt-twa-module', {
-			/* module options */
-			defaultUrl: 'https://your-url.com',
-			hostName: 'your-url.com',
-			applicationId: 'com.example.example',
-			launcherName: 'Your app name',
-			versionCode: 1,
-			versionName: '1.0',
-			statusBarColor: '#F8F8F8',
-			// The sha256Fingerprints by is an array with one SHA-256 key string.
-			// But if you have multiple you can add them to the array. More information about the website asociation:
-			// https://developer.android.com/training/app-links/verify-site-associations#web-assoc
-			sha256Fingerprints: ['/* your SHA-256 keys */'],
-			/* optional */
-			/* overwrite default location for icon */
-			iconPath: '/static/icon.png',
-			/* Overwrite folder where to put .wellknown */
-			distFolder: '.nuxt/dist/client',
-		}],
 	],
 	buildModules: [
 		'@nuxtjs/vuetify',
@@ -64,5 +45,8 @@ module.exports = {
 	},
 	serverMiddleware: [
 		//'~/api'
-	]
+	],
+	router: {
+		base: '/test.pwa/'
+	}
 };
