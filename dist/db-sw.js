eb7d19db13fdaf95a47c2a90bb50230363abce7e
Puld <mr.puld@gmail.com>
2021-02-10 16:56:24 +0300

    clear gh-pages

--- dist/db-sw.js
+++
@@ -1,8 +0,0 @@
-console.log('Custom service worker!')
-
-self.addEventListener('install', function (e) {
-  console.log("install!")
-})
-self.addEventListener('activate', function(event) {
-  console.log("activate!")
-})
